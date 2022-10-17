6305e87698610180f948b58ebcab3a18c0ffa3f2
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2022-10-17 13:50:05 -0700

    impl display

--- obs-client/src/error.rs
+++ obs-client/src/error.rs
@@ -1,5 +1,6 @@
 use graphic_offsets::GraphicOffsetsError;
 use inject_helper::InjectHelperError;
+use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug)]
 pub enum ObsError {
@@ -14,4 +15,24 @@ pub enum ObsError {
     OpenSharedResource,
     CreateTexture,
     MapSurface,
+}
+
+impl Display for ObsError {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            ObsError::ProcessNotFound => write!(f, "ProcessNotFound"),
+            ObsError::Inject(e) => write!(f, "Inject: {:?}", e),
+            ObsError::LoadGraphicOffsets(e) => write!(f, "LoadGraphicOffsets: {:?}", e),
+            ObsError::CreatePipe => write!(f, "CreatePipe"),
+            ObsError::CreateMutex => write!(f, "CreateMutex"),
+            ObsError::CreateEvent => write!(f, "CreateEvent"),
+            ObsError::CreateFileMapping(n) => write!(f, "CreateFileMapping: {:?}", n),
+            ObsError::CreateDevice => write!(f, "CreateDevice"),
+            ObsError::OpenSharedResource => write!(f, "OpenSharedResource"),
+            ObsError::CreateTexture => write!(f, "CreateTexture"),
+            ObsError::MapSurface => write!(f, "MapSurface"),
+        };
+
+        Ok(())
+    }
 }
